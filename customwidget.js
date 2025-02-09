7299ab9c17fcb549667dc996a31d979117aa38ee
AlexeyEm <46366469+AlexeyEm@users.noreply.github.com>
2025-02-09 22:56:33 +0500

    Update customwidget.js

--- customwidget.js
+++ customwidget.js
@@ -1,27 +1,8 @@
 class AgGridCustom extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          display: block;
-          width: 100%;
-          height: 400px;
-          border: 1px solid #ddd;
-        }
-      </style>
-      <div id="gridContainer"></div>
-    `;
-  }
-
-  connectedCallback() {
-    console.log("AG Grid Custom Widget loaded.");
-  }
-
-  setData(data) {
-    console.log("Received data:", data);
-  }
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `<div>Hello, AG Grid!</div>`;
+    }
 }
-
-customElements.define("ag-grid-custom", AgGridCustom);
+customElements.define('ag-grid-custom', AgGridCustom);
