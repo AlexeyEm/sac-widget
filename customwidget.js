0a3c9401d7526b80aaff3cb7151d0d1d11cd76e8
AlexeyEm <46366469+AlexeyEm@users.noreply.github.com>
2025-02-10 20:59:35 +0500

    Update customwidget.js

--- customwidget.js
+++ customwidget.js
@@ -9,30 +9,35 @@ class AgGridCustom extends HTMLElement {
         this.gridDiv.style.width = "100%";
         this.shadowRoot.appendChild(this.gridDiv);
 
+        // Grid Options
         this.gridOptions = {
-            columnDefs: [],
-            rowData: []
+            rowData: [
+                { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+                { make: "Ford", model: "F-Series", price: 33850, electric: false },
+                { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+                { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+                { make: "Fiat", model: "500", price: 15774, electric: false },
+                { make: "Nissan", model: "Juke", price: 20675, electric: false },
+            ],
+            columnDefs: [
+                { field: "make" },
+                { field: "model" },
+                { field: "price" },
+                { field: "electric" },
+            ],
+            defaultColDef: {
+                flex: 1,
+            },
         };
     }
 
-    setData(data) {
+    connectedCallback() {
         if (!window.agGrid || !window.agGrid.createGrid) {
             console.error("AG Grid is not loaded!");
             return;
         }
 
-        this.gridOptions.columnDefs = Object.keys(data[0] || {}).map(key => ({ field: key }));
-        this.gridOptions.rowData = data;
-
-        if (this.gridApi) {
-            this.gridApi.setGridOption('rowData', data);
-        } else {
-            this.gridApi = window.agGrid.createGrid(this.gridDiv, this.gridOptions);
-        }
-    }
-
-    connectedCallback() {
-        this.render();
+        this.gridApi = window.agGrid.createGrid(this.gridDiv, this.gridOptions);
     }
 
     render() {
