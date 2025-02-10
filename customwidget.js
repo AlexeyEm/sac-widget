03042dff85135b85d69af5bf16df6663b382d906
AlexeyEm <46366469+AlexeyEm@users.noreply.github.com>
2025-02-10 21:33:34 +0500

    Update customwidget.js

--- customwidget.js
+++ customwidget.js
@@ -17,9 +17,25 @@ class AgGridCustom extends HTMLElement {
         }
 
         const gridOptions = {
-            rowData: data,
-            columnDefs: Object.keys(data[0] || {}).map(key => ({ field: key })),
-            defaultColDef: { flex: 1 }
+            // Data to be displayed
+            rowData: [
+                { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+                { make: "Ford", model: "F-Series", price: 33850, electric: false },
+                { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+                { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+                { make: "Fiat", model: "500", price: 15774, electric: false },
+                { make: "Nissan", model: "Juke", price: 20675, electric: false },
+            ],
+            // Columns to be displayed (Should match rowData properties)
+            columnDefs: [
+                { field: "make" },
+                { field: "model" },
+                { field: "price" },
+                { field: "electric" },
+            ],
+            defaultColDef: {
+                flex: 1,
+            },
         };
 
         this.gridApi = agGrid.createGrid(this.gridDiv, gridOptions);
