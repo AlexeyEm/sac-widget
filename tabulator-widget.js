1d7f507cfa89d3bf0b8cdc5583e95e8a439aabf0
AlexeyEm <46366469+AlexeyEm@users.noreply.github.com>
2025-02-11 22:09:02 +0500

    Update tabulator-widget.js

--- tabulator-widget.js
+++ tabulator-widget.js
@@ -46,11 +46,25 @@ class TabulatorCustom extends HTMLElement {
             return;
         }
 
+        const columns = [
+    	 	{title:"Name", field:"name", width:150},
+    	 	{title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
+    	 	{title:"Favourite Color", field:"col"},
+    	 	{title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+     	];
+        var tabledata = [
+         	{id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
+         	{id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+         	{id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+         	{id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+         	{id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+         ];
+
         // Инициализация таблицы только после проверки наличия контейнера
         this.table = new Tabulator(this.tableContainer, {
             layout: "fitColumns",
-            columns: [],
-            data: [],
+            columns: columns,
+            data: tabledata,
             autoColumns: true, // Автоматическое создание колонок по данным
             clipboard: "copy",
             clipboardPasteAction: "replace",
