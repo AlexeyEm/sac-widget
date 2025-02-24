d8a2107a63ef55b2a7a7640b86ce09fc2dc72370
AlexeyEm <46366469+AlexeyEm@users.noreply.github.com>
2025-02-24 21:25:38 +0500

    Update jexcel-widget.js

--- jexcel-widget.js
+++ jexcel-widget.js
@@ -1,83 +1,74 @@
-class JExcelCustom extends HTMLElement {
+class JspreadsheetWidget extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-
-    // Создаем контейнер для таблицы
-    this.tableContainer = document.createElement("div");
-    this.tableContainer.style.width = "100%";
-    this.tableContainer.style.height = "100%";
-
-    // Подключаем стили jExcel
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = "https://bossanova.uk/jspreadsheet/v5/jspreadsheet.css";
-    this.shadowRoot.appendChild(link);
-	const link2 = document.createElement("link");
-	link2.rel = "stylesheet";
-    link2.href = "https://jsuites.net/v5/jsuites.css";
-    this.shadowRoot.appendChild(link2);
-
-    // Добавляем контейнер в shadow DOM
-    this.shadowRoot.appendChild(this.tableContainer);
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="https://bossanova.uk/jspreadsheet/v5/jspreadsheet.css">
+      <link rel="stylesheet" href="https://jsuites.net/v5/jsuites.css">
+      <div id="spreadsheet"></div>
+    `;
+    this.spreadsheetInstance = null;
   }
 
-  async connectedCallback() {
-    // Загружаем скрипт jExcel, если он еще не загружен
-    if (!window.jexcel) {
-      await this.loadScript("https://bossanova.uk/jspreadsheet/v5/jspreadsheet.js");
-	  await this.loadScript("https://jsuites.net/v5/jsuites.js");
-    }
+  // Подключение виджета
+  connectedCallback() {
+    this.initializeSpreadsheet();
+  }
 
-    // Инициализация jExcel
-    this.jexcelInstance = jexcel(this.tableContainer, {
-      data: [["", ""], ["", ""]], // Начальные данные
-      columns: [
-        { type: "text", title: "Column 1", width: 150 },
-        { type: "text", title: "Column 2", width: 150 }
-      ],
-      allowInsertRow: true,
-      allowInsertColumn: true,
-      allowDeleteRow: true,
-      allowDeleteColumn: true,
-      copyCompatibility: true,
-      selectionCopy: true
+  // Инициализация Jspreadsheet
+  initializeSpreadsheet() {
+    const spreadsheetDiv = this.shadowRoot.querySelector('#spreadsheet');
+    const data = JSON.parse(this.getAttribute('data') || '[]');
+    const columns = JSON.parse(this.getAttribute('columns') || '[]');
+
+    this.spreadsheetInstance = jspreadsheet(spreadsheetDiv, {
+      worksheets: [{
+        data: data.length ? data : [['', '']],
+        columns: columns.length ? columns : [
+          { type: 'text', title: 'Column 1', width: 200 },
+          { type: 'text', title: 'Column 2', width: 200 }
+        ]
+      }],
+      onchange: () => {
+        this.dispatchEvent(new CustomEvent('onChange', {
+          detail: this.getData()
+        }));
+      }
     });
   }
 
   // Метод для установки данных
   setData(data) {
-    if (this.jexcelInstance) {
-      this.jexcelInstance.setData(data.rows.map(row => row.values));
+    if (this.spreadsheetInstance) {
+      const parsedData = JSON.parse(data);
+      this.spreadsheetInstance[0].setData(parsedData);
     }
   }
 
   // Метод для получения данных
   getData() {
-    return this.jexcelInstance ? this.jexcelInstance.getData() : [];
-  }
-
-  // Обработчик событий изменения данных
-  onDataChange(callback) {
-    if (this.jexcelInstance) {
-      this.jexcelInstance.onchange = (instance, cell, x, y, value) => {
-        callback({ x, y, value });
-      };
+    if (this.spreadsheetInstance) {
+      const data = this.spreadsheetInstance[0].getData();
+      return JSON.stringify(data);
     }
-    return this;
+    return '[]';
   }
 
-  // Загрузка внешнего скрипта
-  loadScript(src) {
-    return new Promise((resolve, reject) => {
-      const script = document.createElement("script");
-      script.src = src;
-      script.onload = resolve;
-      script.onerror = reject;
-      document.head.appendChild(script);
-    });
+  // Обработка изменений атрибутов
+  static get observedAttributes() {
+    return ['data', 'columns'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.spreadsheetInstance && oldValue !== newValue) {
+      if (name === 'data') {
+        this.setData(newValue);
+      } else if (name === 'columns') {
+        this.spreadsheetInstance[0].setConfig({ columns: JSON.parse(newValue) });
+      }
+    }
   }
 }
 
 // Регистрация веб-компонента
-customElements.define("jexcel-custom", JExcelCustom);
+customElements.define('com-example-jspreadsheet-widget', JspreadsheetWidget);
